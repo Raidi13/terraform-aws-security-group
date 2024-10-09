85254621d4857e24c33f336029c32245d37b1dcb
Your Name <you@example.com>
2024-10-09 17:47:04 +0530

    vpc

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "project_name" {
+    default = {}
+}
+
+variable "enivronment" {
+    default = {}
+}
+
+variable "sg_name" {
+    default = {}
+}
+
+variable "sg_tags" {
+    default = {}
+}
+variable "common_tags" {
+    default = {}
+}
+variable "vpc_id" {
+    default = {}
+}
